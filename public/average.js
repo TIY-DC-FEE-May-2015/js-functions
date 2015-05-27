a01f55192b6c3e39797a6c3ace5176d61f921288
Ghaea <ghaea.tam@gmail.com>
2015-05-27 13:39:35 -0400

    sums and averages

--- public/average.js
+++ public/average.js
@@ -3,13 +3,25 @@
   It should return the sum of the members of the array.
 */
 function sum (array) {
-  
-}
 
+  var addition = 0
+  var counter = 0
+
+  while(counter < array.length) {
+
+    addition += array[counter]
+    counter++
+  }
+
+  return addition
+
+}
 /*
   This function should accept an array of numbers as a parameter.
   It should return the average of the members of the array.
 */
 function average (array) {
+
+  return sum(array) / array.length
 
 }
