884f1d5130405071ae628c3ee51dc4161d9a767f
Carlos Jones <carlosjones@Carloss-MBP.home>
2015-05-28 02:32:07 -0400

    First commit

--- public/average.js
+++ public/average.js
@@ -2,14 +2,23 @@
   This function should accept an array of numbers as a parameter.
   It should return the sum of the members of the array.
 */
+var arr = [3, 6, 9, 6]
 function sum (array) {
-  
+	for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+	return sum
 }
 
 /*
   This function should accept an array of numbers as a parameter.
   It should return the average of the members of the array.
 */
+var arr =[3, 6, 9, 6]
 function average (array) {
-
+	var sumOfArray = sum(array);
+	var average = sumOfArray/array.length;
+	 
+	return average
 }
+
+
+
--- public/max.js
+++ public/max.js
@@ -2,18 +2,35 @@
   This function should accept two numbers as parameters.
   It should return the value of the larger number.
 */
-
+var a = 4
+var b = 9
 function max(a, b) {
-
+	if (a > b){
+		return a
+	}
+	else{
+		return b
+	}
+	
+	if (a < b){
+		return b	
+	}
+	else{
+		return a
+	}
+	
+	return max
 }
-
 /*
   This function should accept THREE numbers as parameters.
   It should return the value of the largest number.
   You should make use of the max() function you just wrote.
 */
-
+var c = 10
+var a = 4
+var b = 9
 function maxOfThree(a, b, c) {
-
-}
+   var maxOfThree = Math.max(a, b, c);
+   return maxOfThree
+ }
 
